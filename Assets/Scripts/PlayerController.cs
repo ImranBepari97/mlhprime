01aa2235706bf77aea21a714322ee60250ea174c
Imran Bepari <imranbepari97@gmail.com>
2017-04-01 17:26:39 +0100

    Added sphere continuous rolling and camera following

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    [SerializeField]
+    float forwardMomentum;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        forwardMomentum = 5f;
+    }
+	
+	// Update is called once per frame
+	void Update () { 
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.forward * forwardMomentum);
+    }
+}
