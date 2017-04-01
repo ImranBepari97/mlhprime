5bc785ea8091ef1a952659bf5a4a84e9952ef3c2
Imran Bepari <imranbepari97@gmail.com>
2017-04-01 14:04:25 +0100

    Added spinning cube

---
+++ Assets/Scripts/SpinningCubeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningCubeScript : MonoBehaviour {
+    [SerializeField] int spinSpeed;
+
+	// Use this for initialization
+	void Start () {
+        spinSpeed = 20;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+    }
+}
