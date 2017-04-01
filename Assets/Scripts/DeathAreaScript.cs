bf084e28e791285738dc47c98f2624229d3f063c
Imran Bepari <imranbepari97@gmail.com>
2017-04-01 19:21:39 +0100

    Merge

---
+++ Assets/Scripts/DeathAreaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAreaScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject gameController;
+
+	// Use this for initialization
+	void Start () {
+        gameController = FindObjectOfType<GameController>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           
+        }
+    }
+}
