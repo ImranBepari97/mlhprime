0faa0882fd1f4d3d32fdbe08261198787007286e
Peter Bell <peterbell182@hotmail.co.uk>
2017-04-02 00:46:18 +0100

    Useful

---
+++ Assets/Scripts/Replay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replay : MonoBehaviour {
+
+    public void LoadStage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void LoadEndGame()
+    {
+        SceneManager.LoadScene("EndGame");
+    }
+
+}
+
