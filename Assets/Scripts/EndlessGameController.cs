66b7a3cc9180c49a9c0256cb5be4224cd20b25cd
Imran Bepari <imranbepari97@gmail.com>
2017-04-02 02:34:53 +0100

    Started unlimited stuff

---
+++ Assets/Scripts/EndlessGameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndlessGameController : MonoBehaviour
+{
+    public int score;
+    public bool playing;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        playing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing)
+        {
+            score += 1 + (int)Time.deltaTime;
+        }
+        else
+        {
+            //insert code for game over
+            SceneManager.LoadScene(1);
+        }
+    }
+}
