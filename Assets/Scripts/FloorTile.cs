f1afe642a712b6062a0e37d7a3de556765b8dcc8
Imran Bepari <imranbepari97@gmail.com>
2017-04-02 03:07:33 +0100

    More endless stuff

--- Assets/Scripts/FloorTile.cs
+++ Assets/Scripts/FloorTile.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class FloorTile : MonoBehaviour {
 
+    [SerializeField]
+    float timeLeft;
+    Transform origin;
+    public Transform next;
+    public EndlessGameController endless;
+
 	// Use this for initialization
 	void Start () {
-		
+        timeLeft = 10f;
+        origin = transform.Find("Origin");
+        next = transform.Find("NextTile");
+        transform.Translate(0, 0, -origin.transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0)
+        {
+            Destroy(gameObject);
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("went thru it");
+            endless.createNextTile();
+        }
+    }
 }
